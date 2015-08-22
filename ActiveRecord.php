4919a9e098616049e8c2036b7909e00dcd496624
Steve Guns <Steve@bedezign.com>
2015-08-22 08:01:24 +0200

    Now uses the Yii2 functionality to manage values, as it should. It only keeps track of the names locally

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -1,13 +1,14 @@
 <?php
 /**
  * Just a helper class that allows you to "accept all attributes" without having to define the functions for it every time.
- * All attributes not being assigned to a public property or via a setter are considered "dynamic"
+ * All attributes not being assigned to a public property or via a setter are considered "dynamic".
  *
  * @author    Steve Guns <steve@bedezign.com>
  * @package   com.bedezign.yii2.mongodb
  * @copyright 2014 B&E DeZign
  *
- * @todo Add "parseFromClassDocBlock for the properties
+ * @todo Add "parseFromClassDocBlock" for the properties
+ * @todo Type juggling, since we have the information on a correct docblock
  */
 
 namespace bedezign\yii2\mongodb;
@@ -18,12 +19,30 @@ class ActiveRecord extends \yii\mongodb\ActiveRecord
 	 * @var string List of "dynamically added" (not public properties) attributes
 	 */
 	protected        $attributes  = [];
+	/**
+	 * @var array  "cache" of all models for which the docblock was parsed already
+	 */
 	protected static $_properties = [];
 
+	public function init()
+	{
+		$this->attributes = static::magicProperties();
+		parent::init();
+	}
+
+	/**
+	 * Add an attribute to make sure it is recognised from now on.
+	 * This function is for truly dynamic properties, not even declared in the docblock
+	 *
+	 * @param $name
+	 * @param null $defaultValue
+	 */
 	public function addAttribute($name, $defaultValue = null)
 	{
-		if (!$this->hasAttribute($name))
-			$this->attributes[$name] = $defaultValue;
+		if (!$this->hasAttribute($name)) {
+			$this->attributes[$name] = 'mixed';
+			$this->setAttribute($name, $defaultValue);
+		}
 	}
 
 	public function addAttributes($names)
@@ -33,23 +52,17 @@ class ActiveRecord extends \yii\mongodb\ActiveRecord
 	}
 
 	/**
-	 * Determines if an ActiveRecord has the specified property
-	 *
+	 * Determines if an ActiveRecord has the specified property (in its default mode its pretty useless unless i
 	 * @param string $name
 	 * @param bool $dynamicOnly      if true it will only check the dynamically added attributes, not the public ones
 	 * @return bool
 	 */
 	public function hasAttribute($name, $dynamicOnly = false)
 	{
-		if (array_key_exists($name, $this->attributes))
-			return true;
+		if ($dynamicOnly) {
+			return array_key_exists($name, $this->attributes);
+		}
 		return $dynamicOnly ? false : parent::hasAttribute($name);
-	}
-
-	public function canSetProperty($name, $checkVars = true, $checkBehaviors = true)
-	{
-		if ($this->hasAttribute($name, true)) return true;
-		return parent::canSetProperty($name, $checkVars, $checkBehaviors);
 	}
 
 	/**
@@ -87,34 +100,30 @@ class ActiveRecord extends \yii\mongodb\ActiveRecord
 	}
 
 	/**
-	 * Overridden to make it easier on you, it will automatically return the public properties of the class (+ "_id")
-	 * merged with whatever you allocated dynamically
+	 * Returns all dynamic attributes.
 	 * @return array
 	 */
 	public function attributes()
 	{
-		return array_merge(
-			// Whatever magic was defined
-			array_keys(static::magicProperties()),
-			// Whatever "@runtime" was added
-			array_keys($this->attributes)
-		);
+		return array_keys($this->attributes);
 	}
 
 	/**
 	 * Regular populate overridden to determine the dynamic attributes first and add those.
-	 * After that the populate can continue normally
+	 * After that the populate can continue normally. Please note that this will pickup ANY
+	 * attribute not returned as a regular one, even if it wasn't added as magic property
 	 *
 	 * @param \yii\db\BaseActiveRecord $record
 	 * @param array $row
 	 */
 	public static function populateRecord($record, $row)
 	{
-		// Just figure out the dynamic attributes and then let the code work the regular way
 		if ($row) {
+			// Just figure out the ones we don't already know about and register them so they also get picked up
 			$dynamic = array_diff(array_keys($row), $record->attributes());
 			$record->addAttributes($dynamic);
 		}
+		// Now let the regular code do its job
 		parent::populateRecord($record, $row);
 	}
 }
